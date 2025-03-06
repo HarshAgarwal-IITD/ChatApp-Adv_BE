304250203a84718dd24361752489afbdca5bdbac
HARSH AGARWAL <harshagarwal@HARSHs-MacBook-Air-287.local>
2025-03-06 17:20:27 +0530

    be complete

--- dist/index.js
+++ dist/index.js
@@ -9,22 +9,39 @@ wss.on('connection', (socket) => {
     console.log('userConnected', count);
     socket.on('message', (message) => {
         var _a;
+        let messageToSend = {
+            type: '', message: ""
+        };
         const parsedMsg = JSON.parse(message);
+        if (parsedMsg.type == 'exit') {
+            allSockets.filter(s => s.socket != socket);
+            messageToSend.type = "dialogue";
+            messageToSend.message = "successfully exited the room";
+            socket.send(JSON.stringify(messageToSend));
+        }
         if (parsedMsg.type === 'join') {
             allSockets.push({ socket, room: parsedMsg.payload.roomId });
-            socket.send('successfully joined ' + parsedMsg.payload.roomId);
+            messageToSend.type = "dialogue";
+            messageToSend.message = "successfully joined roomId: " + parsedMsg.payload.roomId;
+            socket.send(JSON.stringify(messageToSend));
+            console.log(messageToSend);
         }
         if (parsedMsg.type == 'chat') {
             let currentUserRoom = (_a = allSockets.find((s) => s.socket == socket)) === null || _a === void 0 ? void 0 : _a.room;
             console.log(parsedMsg);
             const userMessage = parsedMsg.payload.message;
             if (!currentUserRoom) {
-                socket.send('plz join a room first');
+                messageToSend.type = "dialogue";
+                messageToSend.message = "join a room first to send messages";
+                socket.send(JSON.stringify(messageToSend));
             }
             else {
                 allSockets.forEach((u) => {
-                    if (u.room == currentUserRoom)
-                        u.socket.send(userMessage);
+                    if (u.room == currentUserRoom && u.socket !== socket) {
+                        messageToSend.type = "member";
+                        messageToSend.message = userMessage;
+                        u.socket.send(JSON.stringify(messageToSend));
+                    }
                 });
             }
         }
--- src/index.ts
+++ src/index.ts
@@ -12,28 +12,51 @@ wss.on('connection',(socket)=>{
     count++;
     console.log('userConnected',count)
     socket.on('message',(message)=>{
+        let messageToSend={
+            type:'',message:""
+        }
         const parsedMsg = JSON.parse(message as unknown as string);
+        if(parsedMsg.type=='exit'){
+            allSockets.filter(s=>s.socket!=socket)
+            messageToSend.type="dialogue";
+            messageToSend.message="successfully exited the room";
+            socket.send(JSON.stringify(messageToSend));
+            
+        }
         if(parsedMsg.type === 'join')
         {
             allSockets.push({socket, room: parsedMsg.payload.roomId})
-            socket.send('successfully joined ' + parsedMsg.payload.roomId)
+            messageToSend.type="dialogue";
+            messageToSend.message="successfully joined roomId: "+ parsedMsg.payload.roomId;
+            socket.send(JSON.stringify(messageToSend));
+            console.log(messageToSend)
+           
         }
         if(parsedMsg.type=='chat'){
             let currentUserRoom = allSockets.find((s)=>s.socket==socket)?.room;
             console.log(parsedMsg)
             const userMessage= parsedMsg.payload.message;
             if(!currentUserRoom){
-                socket.send('plz join a room first');
+                messageToSend.type="dialogue";
+                messageToSend.message="join a room first to send messages";
+                socket.send(JSON.stringify(messageToSend));
+               
             }
             else{
                 allSockets.forEach((u)=>{
-                   if(u.room == currentUserRoom)
-                    u.socket.send(userMessage);
+                   if(u.room == currentUserRoom && u.socket!==socket){
+                    messageToSend.type="member";
+                    messageToSend.message=userMessage;
+                    u.socket.send(JSON.stringify(messageToSend));
+                   }
+                   
                 })
 
             }
         }
+       
       
-    })
+        })
+  
     
 })
